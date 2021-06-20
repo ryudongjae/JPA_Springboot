3ffbe57e1394d14616dbaef931111765b2f271f1
ryudongjae <rdj10149@gmail.com>
2021-06-20 16:32:25 +0900

    상품 엔티티 개발 + 상품 서비스 개발 (예외 클래스 생성(NotEnoughStockException))

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+//상품서비스는 상품 리포지토리에 단순히 위임만 한는 클래스
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item){
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems(){
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId){
+        return itemRepository.findOne(itemId);
+    }
+}
